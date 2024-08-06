2bc1544d949f59fbc9e0490513a2a1aaf27e41a4
root <root@ip-172-31-10-15.ec2.internal>
2024-08-06 12:43:57 +0000

    add files

---
+++ backend.tf
@@ -0,0 +1,31 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source = "hashicorp/aws"
+
+      version = "~> 4.18.0"
+
+    }
+
+  }
+
+
+
+  backend "s3" {
+
+    bucket = "terraform-niraj"
+
+    key = "state/terraform.tfstate"
+
+    region = "us-east-1"
+
+    encrypt = true
+
+    dynamodb_table = "Terraform_lock"
+
+  }
+
+}
